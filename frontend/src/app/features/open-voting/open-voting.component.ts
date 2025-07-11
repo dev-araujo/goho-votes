5c29bda6b45eeeb14840d33d78a6cc40a222b6fb
araujo <theriancirculo@protonmail.com>
2025-07-10 21:17:59 -0300

    feat(open-voting): Fetch  open polls

--- frontend/src/app/features/open-voting/open-voting.component.ts
+++ frontend/src/app/features/open-voting/open-voting.component.ts
@@ -1,4 +1,11 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import {
+  ChangeDetectionStrategy,
+  Component,
+  DestroyRef,
+  inject,
+} from '@angular/core';
+import { ContractService } from '../../core/services/contract.service';
+import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
 
 @Component({
   selector: 'app-open-voting',
@@ -7,4 +14,31 @@ import { ChangeDetectionStrategy, Component } from '@angular/core';
   styleUrl: './open-voting.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class OpenVotingComponent {}
+export class OpenVotingComponent {
+  openedPolls = [];
+  load = false;
+  private _destroyRef = inject(DestroyRef);
+  private _service = inject(ContractService);
+
+  ngOnInit() {
+    this.getPolls();
+  }
+
+  getPolls() {
+    this._service
+      .getOpenPolls()
+      .pipe(takeUntilDestroyed(this._destroyRef))
+      .subscribe({
+        next: (pools: any) => {
+          this.load = true;
+          this.openedPolls = pools;
+        },
+        error: (err) => {
+          console.error('Ocorreu um erro: ', err);
+        },
+        complete: () => {
+          this.load = false;
+        },
+      });
+  }
+}
